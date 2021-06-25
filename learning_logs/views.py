af61cb99575af9a81e0fb024077dfa874f9b5fc9
Ken V <ken.valverde@yahoo.com>
2021-06-25 17:04:24 -0500

    Fix check_topic_owner

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -10,6 +10,7 @@ def check_topic_owner(request, topic):
     if topic.owner != request.user:
         raise Http404
 
+
 # Create your views here.
 def index(request):
     """The home page for Learning Log."""
@@ -29,7 +30,7 @@ def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
     # Make sure topic belongs to the current user.
-    check_topic_owner()
+    check_topic_owner(request, topic)
 
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
@@ -88,7 +89,6 @@ def edit_entry(request, entry_id):
     if topic.owner != request.user:
         raise Http404
 
-
     if request.method != 'POST':
         # Initial request; pre-fill form with the current entry.
         form = EntryForm(instance=entry)
@@ -101,5 +101,3 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
-
-
